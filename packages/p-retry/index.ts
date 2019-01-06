17407925dc2389df37c7eb30c2772dd352ec4f2f
skt-t1-byungi <tiniwz@gmail.com>
2019-01-06 13:11:05 +0900

    add p-retry

---
+++ packages/p-retry/index.ts
@@ -0,0 +1,37 @@
+import PCancel from '@byungi/p-cancel'
+
+interface CancellablePromise<T> extends PromiseLike<T> {
+    cancel (): void
+}
+
+export const pRetry = <T>(runner: () => PromiseLike<T> | CancellablePromise<T> , { retries = 0, interval = 0 } = {}) =>
+    new PCancel((resolve, reject, onCancel) => {
+        let promise: PromiseLike<T> | CancellablePromise<T>
+        let timer: number
+        let isCanceled = false
+
+        onCancel(() => {
+            isCanceled = true
+            clearTimeout(timer)
+            if (isCancellable(promise)) promise.cancel()
+        })
+
+        const attempt = () => {
+            (promise = runner()).then(resolve, err => {
+                if (isCanceled) return
+                if (retries--) {
+                    timer = setTimeout(attempt, interval)
+                } else {
+                    reject(err)
+                }
+            })
+        }
+
+        attempt()
+    })
+
+export default pRetry
+
+function isCancellable <T> (promise: any): promise is CancellablePromise<T> {
+    return promise && typeof promise.cancel === 'function'
+}
