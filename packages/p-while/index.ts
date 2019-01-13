23abeb24d3f39ad3f0846b8d833c31001b116041
skt-t1-byungi <tiniwz@gmail.com>
2019-01-13 18:25:56 +0900

    type rename

--- packages/p-while/index.ts
+++ packages/p-while/index.ts
@@ -2,14 +2,14 @@ import PCancel from '@byungi/p-cancel'
 import pDelay from '@byungi/p-delay'
 import { isCancellable } from '@byungi/promise-helpers'
 
-type PromiseParam<T> = (() => T) | (() => PromiseLike<T>) | PromiseLike<T> | T
+type Param<T> = (() => T) | (() => PromiseLike<T>) | PromiseLike<T> | T
 
-export const ensurePromiseReturn = <T>(fn: PromiseParam<T>) => () => {
+export const ensurePromiseReturn = <T>(fn: Param<T>) => () => {
     // Function intersections issue : https://github.com/Microsoft/TypeScript/issues/26970
     return Promise.resolve(typeof fn === 'function' ? (fn as (() => T))() : fn)
 }
 
-export const pWhile = <T1,T2>(condition: PromiseParam<T1>, action: PromiseParam<T2>, { interval = 0 } = {}) =>
+export const pWhile = <T1,T2>(condition: Param<T1>, action: Param<T2>, { interval = 0 } = {}) =>
     new PCancel((resolve, reject, onCancel) => {
         const wrappedCondition = ensurePromiseReturn(condition)
         const wrappedAction = ensurePromiseReturn(action)
