ec8ad706ac2dda07cd5012f09b45c5b417dffd7e
skt-t1-byungi <tiniwz@gmail.com>
2019-03-09 17:16:24 +0900

    remove undef type

--- packages/p-class/index.ts
+++ packages/p-class/index.ts
@@ -11,11 +11,11 @@ export class PClass<T> {
         return this._promise
     }
 
-    public then<TR1= T, TR2= never> (onfulfilled?: OnFulfilled<T,TR1> | null | undefined, onrejected?: OnRejected<TR2> | null | undefined) {
+    public then<TR1= T, TR2= never> (onfulfilled?: OnFulfilled<T,TR1> | null , onrejected?: OnRejected<TR2> | null) {
         return this._promise.then(onfulfilled, onrejected)
     }
 
-    public catch<TR = never> (onrejected?: OnRejected<TR> | null | undefined) {
+    public catch<TR = never> (onrejected?: OnRejected<TR> | null) {
         return this._promise.catch(onrejected)
     }
 
